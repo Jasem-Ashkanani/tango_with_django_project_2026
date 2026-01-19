25647fc5b07f7e64008210e44409e5a17cfc9a25
Jasem <2961000A@student.gla.ac.uk>
2026-01-19 16:20:48 +0000

    Chapter 3: index and about views with URL configuration + Passed Chapter 3 Test

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('<h1>Rango says hey there partner!</h1><a href="/rango/about/">About</a>')
+
+def about(request):
+    return HttpResponse('Rango says here is the about page.<br /><a href="/rango/">Index</a>')
